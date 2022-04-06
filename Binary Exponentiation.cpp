0650d7bafa5136d7c5b2cd2aafc4c1105b9f208d
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2022-04-06 22:40:50 +0600

    Create Binary Exponentiation.cpp

---
+++ Binary Exponentiation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("ou.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll a,b,m;
+    cin >> a >> b >> m;
+    cout << binpow(a,b,m) << endl;
+    
+    return 0;
+}
