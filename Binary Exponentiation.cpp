43adc5cf1087a7772ab2a116095670bbbab96bdc
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2022-09-05 21:05:09 +0600

    Update Binary Exponentiation.cpp

--- Binary Exponentiation.cpp
+++ Binary Exponentiation.cpp
@@ -14,7 +14,32 @@ long long binpow(long long a, long long b, long long m) {
     }
     return res;
 }
-
+ double myPow(double x, int n) {
+        long long nn = n;
+        if(nn < 0)
+        {
+            nn = -1*nn;
+        }
+        double ans = 1.0;
+        while(nn != 0)
+        {
+            if(nn%2 != 0)
+            {
+                ans = ans * x;
+                nn = nn - 1;
+            }
+            else
+            {
+                x = x*x;
+                nn /= 2;
+            }
+        }
+        if(n < 0)
+        {
+            return 1/ans;
+        }
+        else return ans;
+    }
 
 int main() 
 {
